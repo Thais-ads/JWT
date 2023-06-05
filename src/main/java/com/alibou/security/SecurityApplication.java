2b9afdb075240691191178e55a71dc5c9f50b97f
Thais Spinoza <thais.spinoza@sintese.local>
2023-06-05 20:28:33 -0300

    primeiro jwt adapt

---
+++ src/main/java/com/alibou/security/SecurityApplication.java
@@ -0,0 +1,63 @@
+package com.alibou.security;
+
+import com.alibou.security.auth.AuthenticationService;
+import com.alibou.security.auth.RegisterRequest;
+import com.alibou.security.user.Role;
+import jakarta.annotation.PostConstruct;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import static com.alibou.security.user.Role.ADMIN;
+import static com.alibou.security.user.Role.MANAGER;
+
+@SpringBootApplication
+public class SecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(
+			AuthenticationService service
+	) {
+		return args -> {
+			var admin = RegisterRequest.builder()
+					.firstname("Admin")
+					.lastname("Admin")
+					.email("admin@mail.com")
+					.password("password")
+					.role(ADMIN)
+					.build();
+			System.out.println("Admin token: " + service.register(admin).getAccessToken());
+
+			var manager = RegisterRequest.builder()
+					.firstname("Admin")
+					.lastname("Admin")
+					.email("manager@mail.com")
+					.password("password")
+					.role(MANAGER)
+					.build();
+			System.out.println("Manager token: " + service.register(manager).getAccessToken());
+
+		};
+	}
+
+	@Configuration
+	public class DatabaseConfiguration {
+
+		@PostConstruct
+		public void disableTlsProtocolCheck() {
+			// Desabilitar a verificação da versão do protocolo TLS
+			System.setProperty("jdk.tls.client.protocols", "TLSv1.0");
+		}
+	}
+
+
+
+
+
+}
